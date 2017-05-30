a6e95dd8b66c9716de53b0bfd33e2ca396379ebb
Sampanna-Sharma <073bct538.sampanna@pcampus.edu.np>
2017-05-30 16:58:38 +0545

    improved

--- Sketcher.py
+++ Sketcher.py
@@ -6,17 +6,26 @@ IMAGE_SORCE = Image.open("photo.jpg")
 
 
 def getdiff(im1, x, y):
+    per_diffA = 0
     l1 = np.array(im1.getpixel((x, y)))
-    l2 = np.array(im1.getpixel((x, y+1)))
-    result = np.abs(np.subtract(l1, l2))
-    diff = np.sum(result)
-    per_diff = (diff / np.sum(l1)) * 100
-    return per_diff
+    for i in range(-1, 1):
+            for j in range(-1, 1):
+                k = y + j
+                l = x + i
+                if k > -1 and k < im1.size[1] and l > -1 and l < im1.size[0]:
+                    l2 = np.array(im1.getpixel((x+i, y+j)))
+                    result = np.abs(np.subtract(l1, l2))
+                    diff = np.sum(result)
+                    per_diff = (diff / np.sum(l1)) * 100
+                    if per_diffA < per_diff:
+                        per_diffA = per_diff
+    return per_diffA
 
 
 def makeskrtch(im1):
     array = []
     for i in range(im1.size[1]-1):
+        print(i/(im1.size[1]-1)*100, "done")
         for j in range(im1.size[0]):
             per_diff = getdiff(im1, j, i)
             if per_diff > 15:
